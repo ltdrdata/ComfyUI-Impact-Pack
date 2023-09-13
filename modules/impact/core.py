7c2038b3d346a49fa734ac425d88cd5b1a6fea62
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-09-13 18:45:36 +0900

    fix: onnx - dilation doesn't work

--- modules/impact/core.py
+++ modules/impact/core.py
@@ -666,8 +666,8 @@ class ONNXDetector:
 
                         # prepare cropped mask
                         cropped_mask = np.zeros((crop_y2 - crop_y1, crop_x2 - crop_x1))
-                        inner_mask = np.ones((y2 - y1, x2 - x1))
-                        cropped_mask[y1 - crop_y1:y2 - crop_y1, x1 - crop_x1:x2 - crop_x1] = inner_mask
+                        cropped_mask[y1 - crop_y1:y2 - crop_y1, x1 - crop_x1:x2 - crop_x1] = 1
+                        cropped_mask = dilate_mask(cropped_mask, dilation)
 
                         # make items
                         item = SEG(None, cropped_mask, scores[i], crop_region, item_bbox, None, None)
