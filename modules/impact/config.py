114a7e84dab16017f85dbcddbedb56d97f36fd9a
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-09-16 20:34:38 +0900

    fix: crash fix about switch nodes

--- modules/impact/config.py
+++ modules/impact/config.py
@@ -2,7 +2,7 @@ import configparser
 import os
 
 
-version = "V4.7"
+version = "V4.7.1"
 
 dependency_version = 11
 
--- modules/impact/impact_server.py
+++ modules/impact/impact_server.py
@@ -210,7 +210,7 @@ def onprompt(json_data):
                 inversed_switch_info[k] = select_input
 
         elif cls in ['ImpactSwitch', 'LatentSwitch', 'SEGSSwitch', 'ImpactMakeImageList']:
-            if v['inputs']['sel_mode']:
+            if 'sel_mode' in v['inputs'] and v['inputs']['sel_mode']:
                 select_input = v['inputs']['select']
                 if isinstance(select_input, list) and len(select_input) == 2:
                     input_node = json_data['prompt'][select_input[0]]
