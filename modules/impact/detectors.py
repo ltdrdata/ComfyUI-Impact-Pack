88d3db9495946256477a8da12a52302dd959aa53
Dr.Lt.Data <dr.lt.data@gmail.com>
2024-02-21 15:40:29 +0900

    improve: SimpleDetectorForEach - better support for YOLO-World

--- modules/impact/detectors.py
+++ modules/impact/detectors.py
@@ -209,16 +209,20 @@ class SimpleDetectorForEach:
         if len(image) > 1:
             raise Exception('[Impact Pack] ERROR: SimpleDetectorForEach does not allow image batches.\nPlease refer to https://github.com/ltdrdata/ComfyUI-extension-tutorials/blob/Main/ComfyUI-Impact-Pack/tutorial/batching-detailer.md for more information.')
 
-        segs = bbox_detector.detect(image, bbox_threshold, bbox_dilation, crop_factor, drop_size, detailer_hook=detailer_hook)
+        if segm_detector_opt is not None and hasattr(segm_detector_opt, 'bbox_detector') and segm_detector_opt.bbox_detector == bbox_detector:
+            # Better segm support for YOLO-World detector
+            segs = segm_detector_opt.detect(image, sub_threshold, sub_dilation, crop_factor, drop_size, detailer_hook=detailer_hook)
+        else:
+            segs = bbox_detector.detect(image, bbox_threshold, bbox_dilation, crop_factor, drop_size, detailer_hook=detailer_hook)
 
-        if sam_model_opt is not None:
-            mask = core.make_sam_mask(sam_model_opt, segs, image, "center-1", sub_dilation,
-                                      sub_threshold, sub_bbox_expansion, sam_mask_hint_threshold, False)
-            segs = core.segs_bitwise_and_mask(segs, mask)
-        elif segm_detector_opt is not None:
-            segm_segs = segm_detector_opt.detect(image, sub_threshold, sub_dilation, crop_factor, drop_size, detailer_hook=detailer_hook)
-            mask = core.segs_to_combined_mask(segm_segs)
-            segs = core.segs_bitwise_and_mask(segs, mask)
+            if sam_model_opt is not None:
+                mask = core.make_sam_mask(sam_model_opt, segs, image, "center-1", sub_dilation,
+                                          sub_threshold, sub_bbox_expansion, sam_mask_hint_threshold, False)
+                segs = core.segs_bitwise_and_mask(segs, mask)
+            elif segm_detector_opt is not None:
+                segm_segs = segm_detector_opt.detect(image, sub_threshold, sub_dilation, crop_factor, drop_size, detailer_hook=detailer_hook)
+                mask = core.segs_to_combined_mask(segm_segs)
+                segs = core.segs_bitwise_and_mask(segs, mask)
 
         segs = core.dilate_segs(segs, post_dilation)
 
