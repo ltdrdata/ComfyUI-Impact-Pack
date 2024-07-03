caa1abbe7b6a5d7b2ad778fbbbcdddca58ad06c8
Dr.Lt.Data <dr.lt.data@gmail.com>
2024-07-04 00:20:23 +0900

    support wildcard refresh in refresh button in new style menu

--- js/impact-wildcard.js
+++ js/impact-wildcard.js
@@ -2,6 +2,7 @@ import { ComfyApp, app } from "../../scripts/app.js";
 import { api } from "../../scripts/api.js";
 
 let refresh_btn = document.getElementById('comfy-refresh-button');
+let refresh_btn2 = document.querySelector('button[title="Refresh widgets in nodes to find new models or files"]');
 
 let orig = refresh_btn.onclick;
 
@@ -9,3 +10,7 @@ refresh_btn.onclick = function() {
 	orig();
 	api.fetchApi('/impact/wildcards/refresh');
 };
+
+refresh_btn2.addEventListener('click', function() {
+	api.fetchApi('/impact/wildcards/refresh');
+});
