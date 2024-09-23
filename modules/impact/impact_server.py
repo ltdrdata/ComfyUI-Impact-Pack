a43dae373e648ae0f0cc0c9768c3cea6a72acff7
Dr.Lt.Data <dr.lt.data@gmail.com>
2024-09-24 01:47:46 +0900

    remove useless code

--- modules/impact/impact_server.py
+++ modules/impact/impact_server.py
@@ -24,37 +24,6 @@ import random
 from server import PromptServer
 
 
-@PromptServer.instance.routes.post("/upload/temp")
-async def upload_image(request):
-    upload_dir = folder_paths.get_temp_directory()
-
-    if not os.path.exists(upload_dir):
-        os.makedirs(upload_dir)
-    
-    post = await request.post()
-    image = post.get("image")
-
-    if image and image.file:
-        filename = image.filename
-        if not filename:
-            return web.Response(status=400)
-
-        split = os.path.splitext(filename)
-        i = 1
-        while os.path.exists(os.path.join(upload_dir, filename)):
-            filename = f"{split[0]} ({i}){split[1]}"
-            i += 1
-
-        filepath = os.path.join(upload_dir, filename)
-
-        with open(filepath, "wb") as f:
-            f.write(image.file.read())
-        
-        return web.json_response({"name": filename})
-    else:
-        return web.Response(status=400)
-
-
 sam_predictor = None
 default_sam_model_name = os.path.join(impact_pack.model_path, "sams", "sam_vit_b_01ec64.pth")
 
