3efdf71caa5db34abc8d2c9c477905fa5f310530
chutchatut <chutchatut@gmail.com>
2025-03-22 13:45:49 +0700

    format code

--- modules/impact/segs_nodes.py
+++ modules/impact/segs_nodes.py
@@ -526,22 +526,14 @@ class SEGSOrderedFilter:
         def sort_key_fn(seg):
             x1, y1, x2, y2 = seg.crop_region
             match target:
-                case "confidence":
-                    return seg.confidence
-                case "area(=w*h)":
-                    return (x2 - x1) * (y2 - y1)
-                case "width":
-                    return x2 - x1
-                case "height":
-                    return y2 - y1
-                case "x1":
-                    return x1
-                case "y1":
-                    return y1
-                case "x2":
-                    return x2
-                case "y2":
-                    return y2
+                case "confidence": return seg.confidence
+                case "area(=w*h)": return (x2 - x1) * (y2 - y1)
+                case "width": return x2 - x1
+                case "height": return y2 - y1
+                case "x1": return x1
+                case "y1": return y1
+                case "x2": return x2
+                case "y2": return y2
                 case _:
                     raise Exception(f"[Impact Pack] SEGSOrderedFilter - Unexpected target '{target}'")
                 
