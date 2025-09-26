b96524ad6c557757fecb5404d476e429f8f02bc1
Kaski <23-23enterprise@gmx.de>
2025-09-26 13:22:19 +0200

    Optimized SEGSPaste to reduce memory usage

--- modules/impact/segs_nodes.py
+++ modules/impact/segs_nodes.py
@@ -165,79 +165,7 @@ class SEGSDetailer:
 
         return segs, cnet_pil_list
 
-"""
-class SEGSPaste:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {"required": {
-                     "image": ("IMAGE", ),
-                     "segs": ("SEGS", ),
-                     "feather": ("INT", {"default": 5, "min": 0, "max": 100, "step": 1}),
-                     "alpha": ("INT", {"default": 255, "min": 0, "max": 255, "step": 1}),
-                     },
-                "optional": {"ref_image_opt": ("IMAGE", ), }
-                }
 
-    RETURN_TYPES = ("IMAGE", )
-    FUNCTION = "doit"
-
-    CATEGORY = "ImpactPack/Detailer"
-
-    DESCRIPTION = "This node provides a function to paste the enhanced SEGS, improved through the SEGS detailer, back onto the original image."
-
-    @staticmethod
-    def doit(image, segs, feather, alpha=255, ref_image_opt=None):
-
-        segs = core.segs_scale_match(segs, image.shape)
-
-        result = None
-        for i, single_image in enumerate(image):
-            image_i = single_image.unsqueeze(0).clone()
-
-            for seg in segs[1]:
-                ref_image = None
-                if ref_image_opt is None and seg.cropped_image is not None:
-                    cropped_image = seg.cropped_image
-                    if isinstance(cropped_image, np.ndarray):
-                        cropped_image = torch.from_numpy(cropped_image)
-                    ref_image = cropped_image[i].unsqueeze(0)
-                elif ref_image_opt is not None:
-                    ref_tensor = ref_image_opt[i].unsqueeze(0)
-                    ref_image = utils.crop_image(ref_tensor, seg.crop_region)
-                if ref_image is not None:
-                    if seg.cropped_mask.ndim == 3 and len(seg.cropped_mask) == len(image):
-                        mask = seg.cropped_mask[i]
-                    elif seg.cropped_mask.ndim == 3 and len(seg.cropped_mask) > 1:
-                        logging.warning(f"[Impact Pack] SEGSPaste: The number of the mask batch({len(seg.cropped_mask)}) and the image batch({len(image)}) are different. Combine the mask frames and apply.")
-                        combined_mask = (seg.cropped_mask[0] * 255).to(torch.uint8)
-
-                        for frame_mask in seg.cropped_mask[1:]:
-                            combined_mask |= (frame_mask * 255).to(torch.uint8)
-
-                        combined_mask = (combined_mask/255.0).to(torch.float32)
-                        mask = utils.to_binary_mask(combined_mask, 0.1)
-                    else:  # ndim == 2
-                        mask = seg.cropped_mask
-
-                    mask = utils.tensor_gaussian_blur_mask(mask, feather) * (alpha/255)
-                    x, y, *_ = seg.crop_region
-
-                    # ensure same device
-                    mask = mask.to(image_i.device)
-                    ref_image = ref_image.to(image_i.device)
-
-                    utils.tensor_paste(image_i, ref_image, (x, y), mask)
-
-            if result is None:
-                result = image_i
-            else:
-                result = torch.concat((result, image_i), dim=0)
-
-        if not args.highvram and not args.gpu_only:
-            result = result.cpu()
-
-        return (result, )
-"""
 
 class SEGSPaste:
     @classmethod
