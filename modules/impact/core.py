096e1056bce900f503fb3e09264717cbc2cb68a8
Jonas Krauss <jonas.krauss@stockpulse.de>
2025-10-04 22:18:26 +0200

    change Differential Diffusion method from apply to execute

--- modules/impact/core.py
+++ modules/impact/core.py
@@ -230,7 +230,7 @@ def enhance_detail(image, model, clip, vae, guide_size, guide_size_for_bbox, max
         noise_mask = noise_mask.squeeze(3)
 
         if noise_mask_feather > 0:
-            model = nodes_differential_diffusion.DifferentialDiffusion().apply(model)[0]
+            model = nodes_differential_diffusion.DifferentialDiffusion().execute(model)[0]
 
     if wildcard_opt is not None and wildcard_opt != "":
         model, _, wildcard_positive = wildcards.process_with_loras(wildcard_opt, model, clip)
@@ -370,7 +370,7 @@ def enhance_detail_for_animatediff(image_frames, model, clip, vae, guide_size, g
         noise_mask = noise_mask.squeeze(3)
 
     if noise_mask_feather > 0:
-        model = nodes_differential_diffusion.DifferentialDiffusion().apply(model)[0]
+        model = nodes_differential_diffusion.DifferentialDiffusion().execute(model)[0]
 
     if wildcard_opt is not None and wildcard_opt != "":
         model, _, wildcard_positive = wildcards.process_with_loras(wildcard_opt, model, clip)
