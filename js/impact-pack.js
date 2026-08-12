dcec1b875a1152e1818c5db2f9073033a42505cf
2322160389 <62737350+2322160389@users.noreply.github.com>
2026-08-12 22:01:58 +0800

    Fix ImpactSwitch frontend reload

--- js/impact-pack.js
+++ js/impact-pack.js
@@ -634,11 +634,6 @@ app.registerExtension({
 						// NOTE: node is undefined when subgraph editing mode
 						if(node) {
 							let origin_type = node.outputs[link_info.origin_slot]?.type;
-							if(link_info.target_slot == 0 && this.inputs.length > 3) {  // NOTE: widgets are regarded as input since new front
-									origin_type = this.inputs[1].type;
-									node.connect(link_info.origin_slot, node.id, 'input1');
-							}
-
 							if(origin_type == '*' && app.graph.getNodeById(link_info.origin_id).slots[link_info.origin_slot].type != '*') {
 								this.disconnectInput(link_info.target_slot);
 								return;
